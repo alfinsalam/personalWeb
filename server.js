fa55eeecb092af5e62f3f6b074cdc23ca73adde0
unknown <alfinabdussalam03@gmail.com>
2025-02-11 23:06:28 +0700

    update 12 februari

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require ("express");
+const path = require("path");
+const hbs = require("hbs")
+
+const app = express();
+const PORT = 5000
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use('/assets', express.static(path.join(__dirname, "./assets")))
+
+hbs.registerPartials(__dirname + "/views/partials", function(err) {})
+
+app.get("/index", (req, res) => {
+  // res.send ("test");
+  res.render("index")
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+app.get("/testimonial", (req, res) => {
+  res.render("testimonial");
+});
+
+app.get("/:lang/project/:id", (req, res) => {
+  const { id, lang } = req.params;
+  const {name, tittle} = req.query;
+})
+
+app.listen(PORT, () => {
+  console.log(`server berjalan di ${PORT}`);
+});
